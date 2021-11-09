7e004e48c19e12922001f9faf915e41d0c842a49
Akhilesh Singh <akhisingh6871@gmail.com>
2021-11-10 00:30:03 +0530

    Larget BST in a Binary Tree

---
+++ Tree/46. Largest BST in a Binary Tree.cpp
@@ -0,0 +1,56 @@
+/* Tree node structure  used in the program
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};*/
+
+class helperNode {
+    public :
+    int maxi, mini, node_cnt;
+    
+    helperNode(int maxi, int mini, int node_cnt) {
+        this->maxi = maxi;
+        this->mini = mini;
+        this->node_cnt = node_cnt;
+    }
+};
+
+class Solution{
+    private:
+    helperNode largestBstSubtreeHelper(Node* root) {
+        if(root == NULL) return helperNode(INT_MIN, INT_MAX, 0);
+        
+        // get the values from the left and right subtree from the current node:
+        
+        helperNode left = largestBstSubtreeHelper(root->left);
+        helperNode right = largestBstSubtreeHelper(root->right);
+        
+        // If current node is Greater than the Max node of left AND smaller than
+        // the Min node of the right subtree then it is a Valid BST
+        if(root->data > left.maxi && root->data < right.mini) {
+            
+            return helperNode(max(root->data, right.maxi), min(root->data, left.mini), (1 + left.node_cnt + right.node_cnt));
+            
+        }
+        // otherwise it will not be BST, hence return the values which ill not be comparabale further
+        else {
+            return helperNode(INT_MAX, INT_MIN, max(left.node_cnt, right.node_cnt));
+        }
+        
+    }
+    
+    public:
+    /*You are required to complete this method */
+    // Return the size of the largest sub-tree which is also a BST
+    int largestBst(Node *root)
+    {
+    	return largestBstSubtreeHelper(root).node_cnt;
+    }
+};
